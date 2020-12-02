3bd450f378b6557590b5f1da59f9cda294866bdf
Boris Rumyantsev <bdrum@jinr.ru>
2020-12-02 10:53:55 +0100

    Visible Cross section has done. Close #32

--- notebooks/modules/__init__.py
+++ notebooks/modules/__init__.py
@@ -12,8 +12,8 @@ events = uproot4.open(ccup9_2015)['4Prongs/events']
 # events.show()
 # dfs.loc[0] # get dataframe part with entry = 0
 
-dfs = events.arrays(filter_name=['T_Px', 'T_Py', 'T_Pz',  'T_Q',
-                                 'T_NumberOfSigmaTPCPion', 'T_TPCRefit', 'T_TPCNCls', 'T_Phi', 'T_Eta'], library='pd')
+# dfs = events.arrays(filter_name=['T_Px', 'T_Py', 'T_Pz',  'T_Q',
+#                                  'T_NumberOfSigmaTPCPion', 'T_TPCRefit', 'T_TPCNCls', 'T_Phi', 'T_Eta'], library='pd')
 
 Rho0 = Particle.from_pdgid(113)
 Pi0 = Particle.from_pdgid(111)
--- notebooks/modules/physics/analysis/ITSvsTPC_events.py
+++ notebooks/modules/physics/analysis/ITSvsTPC_events.py
@@ -2,7 +2,7 @@ from modules import plt, hep
 from matplotlib.patches import Rectangle
 
 
-def ShowComparisonSame(title, arrs, xlabel, labels, colors, nBins=100, ranges=(0, 1)):
+def ShowComparisonSame(title, arrs, xlabel, labels, colors, nBins=100, ranges=(0, 1), showPatch=False):
 
     l = len(arrs)
 
@@ -10,7 +10,7 @@ def ShowComparisonSame(title, arrs, xlabel, labels, colors, nBins=100, ranges=(0
         colors = ['black']*l
 
     plt.style.use(hep.style.ROOT)
-    fig = plt.figure(figsize=(15, 7))
+    fig = plt.figure(figsize=(20, 10))
     fig.suptitle(title, fontsize=32)
 
     ax = fig.add_subplot()
@@ -18,7 +18,11 @@ def ShowComparisonSame(title, arrs, xlabel, labels, colors, nBins=100, ranges=(0
     for i in range(l):
         ax.hist(arrs[i], bins=nBins, range=ranges,
                 color=colors[i], histtype='step', linewidth=2, label=labels[i]+f' | {(arrs[i] < 0.15).sum()}')
-        ax.legend(prop={'size': 10})
+        ax.legend(prop={'size': 14})
 
-    ax.set_xlabel(xlabel)
-    # ax.add_patch(Rectangle((0, 0.15), 0.15, 900, fc='lightgrey', alpha=0.4))
+        ax.set_xlabel(xlabel)
+
+    if (showPatch):
+        ax.add_patch(Rectangle((0, 0.15), 0.15, 900,
+                               fc='lightgrey', alpha=0.4))
+        ax.text(0.15, 0, "0.15", size=14)
