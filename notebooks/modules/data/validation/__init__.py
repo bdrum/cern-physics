de4f3d4ea753be47e30c5fdae5197851a7dad14d
Boris Rumyantsev <bd.rumyantsev@gmail.com>
2021-04-23 17:26:58 +0300

    Forgotten commit

--- notebooks/modules/data/validation/__init__.py
+++
@@ -1,8 +0,0 @@
-from pandas import DataFrame
-
-# TODO: in case of classes I can add _getattr__ and put validation there...
-
-
-def IsDataFrame(tracks):
-    if (type(tracks) != DataFrame):
-        raise AttributeError('Input data should be a pandas DataFrame')
