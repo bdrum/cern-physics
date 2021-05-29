275aeb14967301fbd633f24b6f7a38c2a31dc818
Boris Rumyantsev <bd.rumyantsev@gmail.com>
2021-05-29 20:19:49 +0200

    Wrap histogram with error bar to separate function

--- notebooks/FourTracks/vis/ErrorHist.py
+++ notebooks/FourTracks/vis/ErrorHist.py
@@ -1,40 +1,73 @@
+from matplotlib.pyplot import xlabel
 from FourTracks import pd, np, plt, hep
+from typing import Sized, Union
 
 
-def DrawErrorHist(data: list, b: int, range: tuple, title: str, x_label: str):
+def DrawErrorHists(
+    data: list,
+    bins: int,
+    range: tuple,
+    title: str,
+    label: list,
+    x_label: list,
+    y_label: list,
+    color: list,
+    showBinWidth: bool = True,
+) -> pd.DataFrame:
+    """
+    """
+
+    # if (
+    #     pd.DataFrame([data, label, x_label, y_label, color]).count(axis=1).nunique()
+    #     != 1
+    # ):
+    #     raise IndexError("Input lists have different sizes")
+
     plt.style.use(hep.style.ROOT)
     fig = plt.figure()
     ax = fig.add_subplot()
-
     fig.suptitle(title)
-    counts, bin_edges = np.histogram(total_heavy_pairs_masses, bins=b, range=r)
+
+    for i in np.arange(len(data)):
+        hep.histplot(
+            np.histogram(data[i], bins=bins, range=range),
+            yerr=True,
+            color=color[i],
+            histtype="errorbar",
+            label=label[i],
+        )
+
+        val = (range[1] - range[0]) * 1000 // bins
+        ax.set_xlabel(x_label[i])
+        ylabel = y_label[i]
+        if showBinWidth:
+            ylabel = ylabel + "/" + f"{val}MeV"
+        ax.set_ylabel(ylabel)
+
+    _ = plt.legend()
+    return pd.DataFrame(map(pd.DataFrame.describe, data), index=label)
 
 
-hep.histplot(
-    (counts, bin_edges),
-    yerr=True,
-    color="black",
-    histtype="errorbar",
-    label="4tr_zq_all_cuts",
-)
-
-val = (r[1] - r[0]) * 1000 // b
-ax.set_xlabel("$M, GeV$")
-ax.set_ylabel(f"#events / {val}MeV")
-
-mass_nzq = mass_events(kinematics.GetTracksWithPtCut(data.four_tracks_nzq))
-counts_bckg, bin_edges_bckg = np.histogram(total_light_pairs_masses, bins=b, range=r)
-hep.histplot(
-    (counts_bckg, bin_edges_bckg),
-    yerr=True,
-    color="red",
-    histtype="errorbar",
-    label="4tr_nzq_pt_cut",
-)
-_ = plt.legend()
-
-pd.DataFrame(
-    [mass_after_all_cuts.describe(), mass_nzq.describe()],
-    index=["mass_ft_zq", "mass_ft_nzq"],
-)
+def DrawErrorHist(
+    data: Union[list, pd.Series, np.ndarray],
+    bins: int,
+    range: tuple,
+    title: str,
+    label: str,
+    x_label: str,
+    y_label: str,
+    color: str,
+    showBinWidth: bool = True,
+):
+    return DrawErrorHists(
+        [data],
+        bins=bins,
+        range=range,
+        title=title,
+        label=[label],
+        x_label=[x_label],
+        y_label=[y_label],
+        color=[color],
+        showBinWidth=showBinWidth,
+    )
 
